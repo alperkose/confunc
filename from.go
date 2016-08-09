a9657b95d189354aaead8c9ef083a20cfaa1c5d1
alperkose <alperkose@gmail.com>
2016-08-09 10:26:33 +0300

    adds integer support

--- from.go
+++ from.go
@@ -1,10 +1,12 @@
 package confunc
 
+import "strconv"
+
 type Parameterizer struct {
 	s Source
 }
 
-func (p *Parameterizer) String(sourceKey string, interceptors ...Interceptor) String{
+func (p *Parameterizer) String(sourceKey string, interceptors ...Interceptor) String {
 
 	base := func() string {
 		return p.s.Value(sourceKey)
@@ -18,12 +20,22 @@ func (p *Parameterizer) String(sourceKey string, interceptors ...Interceptor) St
 
 }
 
-func convertInterceptor(i Interceptor, base String) String{
+func (p *Parameterizer) Int(sourceKey string, interceptors ...Interceptor) Integer {
+	base := func() int {
+		strFunc := p.String(sourceKey, interceptors...)
+		val, _ := strconv.Atoi(strFunc())
+		return val
+	}
+
+	return base
+}
+
+func convertInterceptor(i Interceptor, base String) String {
 	return func() string {
 		return i(base)
 	}
 }
 
-func From(s Source) *Parameterizer{
+func From(s Source) *Parameterizer {
 	return &Parameterizer{s}
 }
---
+++ int_test.go
@@ -0,0 +1,38 @@
+package confunc_test
+
+import (
+	"github.com/alperkose/confunc"
+	"testing"
+	"strconv"
+)
+
+func Test_IntFunc(t *testing.T) {
+
+	expectedValue := 17
+	configurationKey := "myConfig"
+	configUnderTest := confunc.
+		From(confunc.Map(map[string]string{configurationKey: strconv.Itoa(expectedValue)})).
+		Int(configurationKey)
+
+	actualValue := configUnderTest()
+	if actualValue != expectedValue {
+		t.Errorf("expected '%v' to be '%v'", actualValue, expectedValue)
+	}
+}
+
+func Test_IntFuncWithWrapper(t *testing.T) {
+
+	aValue := 17
+	expectedValue := 25
+	configurationKey := "myConfig"
+	configUnderTest := confunc.
+		From(confunc.Map(map[string]string{configurationKey: strconv.Itoa(aValue)})).
+		Int(configurationKey, func(s confunc.String) string {
+			return strconv.Itoa(expectedValue)
+	})
+
+	actualValue := configUnderTest()
+	if actualValue != expectedValue {
+		t.Errorf("expected '%v' to be '%v'", actualValue, expectedValue)
+	}
+}
