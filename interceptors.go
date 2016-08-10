67cdc4079e45d7abfbeea754b5aa909969c517e7
alperkose <alperkose@gmail.com>
2016-08-10 11:27:15 +0300

    adds cacheFor interceptor that maintains cache for a given duration

--- interceptors.go
+++ interceptors.go
@@ -1,8 +1,11 @@
 package confunc
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
-func CacheOnceInterceptor() Interceptor {
+func CacheOnce() Interceptor {
 	var o sync.Once
 	var cached string
 
@@ -10,6 +13,22 @@ func CacheOnceInterceptor() Interceptor {
 		o.Do(func() {
 			cached = v()
 		})
+		return cached
+	}
+}
+
+func CacheFor(d time.Duration) Interceptor {
+	var lastCall time.Time = time.Now().Add(-10*time.Second - d)
+	var m sync.Mutex
+	var cached string
+
+	return func(v String) string {
+		m.Lock()
+		if time.Since(lastCall) > d {
+			lastCall = time.Now()
+			cached = v()
+		}
+		m.Unlock()
 		return cached
 	}
 }
--- interceptors_test.go
+++ interceptors_test.go
@@ -9,12 +9,38 @@ import (
 )
 
 func Test_CacheOnceInterceptor(t *testing.T) {
-	interceptorUnderTest := confunc.CacheOnceInterceptor()
+	interceptorUnderTest := confunc.CacheOnce()
 
 	firstVal := interceptorUnderTest(someRandomStuff)
 	for i := 0; i < 100; i++ {
 		aVal := interceptorUnderTest(someRandomStuff)
 		if firstVal != aVal {
+			t.Errorf("expected '%v' to be '%v'", firstVal, aVal)
+		}
+	}
+}
+
+func Test_CacheForInterceptor(t *testing.T) {
+	var cacheOffset = 250 * time.Millisecond
+	interceptorUnderTest := confunc.CacheFor(cacheOffset)
+
+	firstVal := interceptorUnderTest(someRandomStuff)
+	for i := 0; i < 100; i++ {
+		aVal := interceptorUnderTest(someRandomStuff)
+		if firstVal != aVal {
+			t.Errorf("expected '%v' to be '%v'", firstVal, aVal)
+		}
+	}
+
+	time.Sleep(cacheOffset)
+
+	lastVal := interceptorUnderTest(someRandomStuff)
+	if firstVal == lastVal {
+		t.Errorf("expected '%v' to be different than '%v'", lastVal, firstVal)
+	}
+	for i := 0; i < 100; i++ {
+		aVal := interceptorUnderTest(someRandomStuff)
+		if lastVal != aVal {
 			t.Errorf("expected '%v' to be '%v'", firstVal, aVal)
 		}
 	}
